278f274589d426865a47e6f4451d72d405b2e7ac
Darshan <darshannas11@gmail.com>
2023-02-25 20:09:53 +0530

    CORS error solved

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.use(express.json());
 app.use(cors({
     origin: '*'
 }))
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+  });
 app.use(cookieParser())
 
 app.get('/', (req, res) => {
