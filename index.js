17b282581f546ad4470e2612f645e304cf83b365
Darshan <darshannas11@gmail.com>
2023-02-26 20:29:54 +0530

    body

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 const { connection } = require('./configs/db.js');
 
