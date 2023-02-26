45b197a69fde51255c07a4eda6d2be7095982369
Darshan <darshannas11@gmail.com>
2023-02-26 21:25:40 +0530

    cors

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ require('dotenv').config()
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin: '*'
-}))
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
